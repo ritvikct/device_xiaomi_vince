44136d6a6ed4b235afec2583f4d7a7b70e024b21
GhostMaster69-dev <rathore6375@gmail.com>
2022-04-29 21:27:05 +0530

    vince: Remove CarrierConfig from RRO Exlude list
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: ritvik-ch <ritvikchandel06@gmail.com>

--- device.mk
+++ device.mk
@@ -37,6 +37,10 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
+# RRO (Runtime Resource Overlay)
+PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Snap
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
