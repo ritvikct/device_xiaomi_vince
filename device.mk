aff3d7db8cf603500741f4433dc74b6c8ae21581
ritvikct <ritvikct@proton.me>
2024-12-06 14:15:35 +0530

    vince: drop audio.a2dp
    Causing compiling errors

--- device.mk
+++ device.mk
@@ -112,7 +112,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.1-impl \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msm8953 \
     audio.r_submix.default \
