e9dff76c062144b938163fbb46184372e31d0775
ritvikct <ritvikct@proton.me>
2024-12-06 15:44:21 +0530

    vince: aospify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_vince.mk
+    $(LOCAL_DIR)/aosp_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_vince-user \
-    blaze_vince-userdebug \
-    blaze_vince-eng
+    aosp_vince-user \
+    aosp_vince-userdebug \
+    aosp_vince-eng
