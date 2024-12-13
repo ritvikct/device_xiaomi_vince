deb0d877dab62383030348d46b4ebfa0767bfdbf
ritvikct <ritvikct@proton.me>
2024-12-13 09:29:06 +0000

    vince: remove packages
    Signed-off-by: ritvikct <ritvikct@proton.me>

--- removepackages/Android.mk
+++ removepackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube Videos RecorderPrebuilt TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2
+LOCAL_OVERRIDES_PACKAGES := Aperture Chrome Maps Drive Photos Gmail Markup MusicFX Youtube Calculator Clock Gallery Calendar Snap Browser
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
