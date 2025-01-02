edeb9a1e6158d62ec4b7f927c6c521857bf7d50c
ritvikct <ritvikct@proton.me>
2025-01-02 14:13:07 +0000

    vince: add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone git clone https://github.com/ProjectBlaze-Devices/vendor_xiaomi_vince.gi vendor/xiaomi/vince
+git clone https://github.com/GhostMaster69-dev/android_kernel_xiaomi_vince.git kernel/xiaomi/vince
+git clone https://gitlab.com/anandhan07/aosp-clang.git prebuilts/clang/host/linux-x86/clang-aosp
