94d3365494db939c8b6e4e7a72c81cbcaed8ed33
ritvik <ritvikct@proton.me>
2025-07-07 09:11:24 +0530

    vince: remove more packages

--- removepackages/Android.mk
+++ removepackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Aperture Chrome Maps Drive Photos Gmail Markup MusicFX Youtube Calculator Clock Gallery Calendar Snap Browser
+LOCAL_OVERRIDES_PACKAGES := Aperture Chrome Maps Drive Photos Gmail Markup MusicFX Youtube Calculator Clock Gallery Calendar Snap Browser RecorderPrebuilt arcore Camera2 AndroidAutoStubPrebuilt Seedvault TipsPrebuilt 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
