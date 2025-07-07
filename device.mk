fe78381299870b93560035431ff22464c89bf14f
ritvik <ritvikct@proton.me>
2025-07-08 01:28:23 +0530

    vince: remove offending entries

--- device.mk
+++ device.mk
@@ -131,8 +131,6 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    libldacBT_dec \
-    libbt-vendor \
     vendor.qti.hardware.btconfigstore@1.0 \
     vendor.qti.hardware.btconfigstore@2.0 \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
@@ -183,21 +181,16 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    copybit.msm8953 \
     gralloc.msm8953 \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libdisplayconfig \
-    libdisplayconfig.qti \
     libvulkan \
-    liboverlay \
     libqdMetaData.system \
-    libgenlock \
     libtinyxml \
     vendor.display.config@1.0.vendor
 
@@ -205,7 +198,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.3-service-lazy.clearkey \
     android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.2.vendor \
@@ -248,10 +240,8 @@ PRODUCT_PACKAGES += \
     libwifi-hal-ctrl \
     android.hardware.gnss@1.0.vendor \
     android.hardware.gnss@1.1.vendor \
-    android.hardware.gnss@1.2.vendor \
     android.hardware.gnss@2.0.vendor \
-    android.hardware.gnss@2.1.vendor \
-    android.hardware.gnss@3.0.vendor
+    android.hardware.gnss@2.1.vendor
 
 #HALS
 PRODUCT_SOONG_NAMESPACES += \
@@ -269,9 +259,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
+    android.hidl.manager@1.0
 
 # IORAP Feature -- Imported from LA.QSSI.12.0.r1-07100.01-qssi.0
 PRODUCT_SYSTEM_PROPERTIES += \
@@ -357,7 +345,6 @@ PRODUCT_PACKAGES += \
     libOmxEvrcEnc \
     libOmxG711Enc \
     libOmxQcelp13Enc \
-    libOmxSwVencHevc \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
@@ -375,8 +362,7 @@ PRODUCT_COPY_FILES += \
 
 # QMI
 PRODUCT_PACKAGES += \
-    libjson \
-    libjson.vendor
+    libjson
 
 # QTI
 PRODUCT_PACKAGES += \
@@ -415,9 +401,6 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0.vendor \
     android.system.net.netd@1.1.vendor \
     librmnetctl \
-    librmnetctl.vendor \
-    libtinyxml.vendor \
-    libcnefeatureconfig \
     libxml2 \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
@@ -452,15 +435,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libdpmframework_shim
 
-# TextClassifier smart selection model files
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.msm8953
+    android.hardware.thermal@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
